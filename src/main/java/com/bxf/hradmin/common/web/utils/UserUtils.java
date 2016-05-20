4439c0b14526370705047d48ca1cbd198427cc3e
Bo-Xuan Fan <coffee0127@gmail.com>
2016-05-20 14:08:36 +0800

    dummy logged-in user

---
+++ src/main/java/com/bxf/hradmin/common/web/utils/UserUtils.java
@@ -0,0 +1,54 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Bo-Xuan Fan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.bxf.hradmin.common.web.utils;
+
+import org.springframework.security.core.AuthenticationException;
+
+import com.bxf.hradmin.aamgr.dto.UserDto;
+
+/**
+ * UserUtils
+ *
+ * @since 2016-05-12
+ * @author Bo-Xuan Fan
+ */
+public final class UserUtils {
+
+    private UserUtils() {
+    }
+
+    /**
+     * get User object
+     *
+     * @return user object
+     * @throws AuthenticationException
+     */
+    public static UserDto getUser() throws AuthenticationException {
+        // TODO get user from Spring Security
+        UserDto userDto = new UserDto();
+        userDto.setUserName("王曉明");
+        userDto.setUserIp("192.168.57.21");
+        return userDto;
+    }
+}
