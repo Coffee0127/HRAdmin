941faf56ae2d6b6b5966827dcbcb636435e9f03d
Bo-Xuan Fan <coffee0127@gmail.com>
2016-06-13 18:39:32 +0800

    add common.css, query-cond.css, common.properties.js

---
+++ src/main/webapp/resources/js/common/common.properties.js
@@ -0,0 +1,20 @@
+Properties = {
+	CaseStatus: {
+	    /** 已收件狀態 **/
+	    RECEIVED_CASE_STATUS: '01',
+	    /** 已申請狀態 **/
+	    PASSED_CASE_STATUS: '02',
+	    /** 未通過狀態 **/
+	    UNPASSED_CASE_STATUS: '03',
+	    /** 已回覆狀態 **/
+	    RESPONSE_CASE_STATUS: '04',
+	    /** 處理中狀態 **/
+	    HANDLING_CASE_STATUS: '05',
+	    /** 已結案狀態 **/
+	    CLOSE_CASE_STATUS: '10',
+	    /** 案件異常狀態 **/
+	    EXCEPTION_CASE_STATUS: '97',
+	    /** 放棄申請狀態 **/
+	    DISPOSED_CASE_STATUS: '98'
+	}
+};
