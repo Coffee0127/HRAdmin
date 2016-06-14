9e5ce0a1bfbc743c377927764ee9ddc71dbb827b
Bo-Xuan Fan <coffee0127@gmail.com>
2016-06-14 16:15:52 +0800

    trim empty string to null

--- src/main/java/com/bxf/hradmin/headcount/controller/HeadcountController.java
+++ src/main/java/com/bxf/hradmin/headcount/controller/HeadcountController.java
@@ -30,9 +30,12 @@ import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -60,6 +63,12 @@ public class HeadcountController {
 
     @Autowired
     private CaseMgrService service;
+
+    @InitBinder
+    public void dataBinding(WebDataBinder binder) {
+        binder.registerCustomEditor(String.class, "reason", new StringTrimmerEditor(true));
+        binder.registerCustomEditor(String.class, "note", new StringTrimmerEditor(true));
+    }
 
     @RequestMapping("/apply")
     public ModelAndView applyIndex(Model model) {
