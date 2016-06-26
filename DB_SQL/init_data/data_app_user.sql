e1a5d37dcf123f710b296ceb2d2fe8739fc04ef0
Bo-Xuan Fan <coffee0127@gmail.com>
2016-06-26 15:40:07 +0800

    add user1 for ROLE_ADMIN

--- DB_SQL/init_data/data_app_user.sql
+++ DB_SQL/init_data/data_app_user.sql
@@ -1,3 +1,4 @@
+INSERT INTO APP_USER (ACCOUNT, NAME, EMAIL, STATUS) VALUES ('user1', 'user1', 'coffee0127@gmail.com', 'Y');
 INSERT INTO APP_USER (ACCOUNT, NAME, EMAIL, STATUS) VALUES ('user2', 'user2', 'coffee0127@gmail.com', 'Y');
 INSERT INTO APP_USER (ACCOUNT, NAME, EMAIL, STATUS) VALUES ('user21', 'user21', 'coffee0127@gmail.com', 'Y');
 INSERT INTO APP_USER (ACCOUNT, NAME, EMAIL, STATUS) VALUES ('user3', 'user3', 'coffee0127@gmail.com', 'Y');
